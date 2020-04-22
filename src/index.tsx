8c0bcf3d59b1305ab340c4a556f0a9a16ca8f952
Parris Khachi <parris@storyforj.com>
2020-04-22 13:52:25 -0700

    Scaffold for routing and async loading of pages
    We only have 2 pages. This is a little overkill, but I guess I wanted to
    show what a real world app like this would look like. We have a router
    to switch between pages, and we have async loading to reduce bundle
    size. I don't think bundle size will be toooo much of problem in an app
    like this, but once again just demoing some architecture thought
    process.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import './index.css';
-import Home from './pages/Home';
 import { createStore } from './utils/createStore';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import Loadable from 'react-loadable';
+
+// I don't like that we use the <any, any> type for Loadable.OptionsWithRender, but it's better than
+// importing the props each of these components, which defeats the whole point of the async loading.
+const ViewGiphyImage = Loadable({
+  loader: () => import('./pages/ViewGiphyImage'),
+} as Loadable.OptionsWithRender<any, any>);
+const Home = Loadable({
+  loader: () => import('./pages/Home'),
+} as Loadable.OptionsWithRender<any, any>);
 
 const startApp = () => {
   const store = createStore();
   ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
-        <Home />
+        <Router>
+          <Switch>
+            <Route path="/about">
+            </Route>
+            <Route path="/view/:id">
+              <ViewGiphyImage />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
       </Provider>
     </React.StrictMode>,
     document.getElementById('root')
---
+++ src/pages/ViewGiphyImage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ViewGiphyImage() {
+  return (
+    <div>
+      View
+    </div>
+  );
+}
+
+export default ViewGiphyImage;
